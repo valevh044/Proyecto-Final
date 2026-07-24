171354b83ae151d0b3e3191b480bed0da650a246
Jonathan Centeno <jonathannncenteno@gmail.com>
2026-07-24 17:44:36 -0600

    prueba jonathan 2

--- OneDrive - Universidad Fidélitas/Progra Tercer cuatri/Semana 12/ProyectoFinal/src/Controlador/ControladorUsuarios.java
+++ OneDrive - Universidad Fidélitas/Progra Tercer cuatri/Semana 12/ProyectoFinal/src/Controlador/ControladorUsuarios.java
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import java.io.FileOutputStream;
 
+//prueba jona
 public class ControladorUsuarios {
 
     private Usuarios modeloUsuario;
